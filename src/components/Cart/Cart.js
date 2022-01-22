463b135c64646f2af932dde95b5c8e1884dca135
anubhavc9 <anubhavc9@github.com>
2022-01-22 17:41:50 +0530

    managing Cart & Modal state

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,14 +10,16 @@ const Cart = (props) => {
     </ul>
   );
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className='total'>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className='actions'>
-        <button className='button--alt'>Close</button>
+        <button className='button--alt' onClick={props.onClose}>
+          Close
+        </button>
         <button className='button'>Order</button>
       </div>
     </Modal>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className='header'>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className='main-image'>
         <img src={mealsImage} alt='A table full of delicious food!' />
