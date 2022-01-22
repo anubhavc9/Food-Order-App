7c517d99b3e4b1a81291738658a11be18a842b1d
anubhavc9 <anubhavc9@github.com>
2022-01-22 17:18:46 +0530

    added Modal via "React Portals"

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import './Cart.css';
 
 const Cart = (props) => {
@@ -9,7 +10,7 @@ const Cart = (props) => {
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className='total'>
         <span>Total Amount</span>
@@ -19,7 +20,7 @@ const Cart = (props) => {
         <button className='button--alt'>Close</button>
         <button className='button'>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
