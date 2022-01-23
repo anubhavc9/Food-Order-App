572e465eabf7d4f5989bb822b3ece5cbd948458e
anubhavc9 <anubhavc9@github.com>
2022-01-23 17:39:40 +0530

    minor bug fixed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -22,8 +22,8 @@ const Cart = (props) => {
           name={item.name}
           amount={item.amount}
           price={item.price}
-          onRemove={cartItemRemoveHandler(null, item.id)}
-          onAdd={cartItemAddHandler(null, item)}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
