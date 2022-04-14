5a7fa34bab5fc637defc03f93f04781796a56715
Anubhav <anubhavc9@gmail.com>
2022-04-14 16:03:06 +0530

    Changed header css file to css module

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import mealsImage from '../../assets/meals.jpg';
-import './Header.css';
+import classes from './Header.module.css';
 import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <React.Fragment>
-      <header className='header'>
-        <h1>ReactMeals</h1>
+      <header className={classes.header}>
+        <h1>Food Order</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
-      <div className='main-image'>
+      <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food!' />
       </div>
     </React.Fragment>
