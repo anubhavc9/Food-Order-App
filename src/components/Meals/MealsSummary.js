d0eae2d427a5efe677dda6de18f54378f624186e
Anubhav <anubhavc9@gmail.com>
2022-04-15 19:48:45 +0530

    Dynamically changing main heading after every 5sec

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,16 +1,32 @@
+import { useCallback, useEffect, useState } from 'react';
 import classes from './MealsSummary.module.css';
 
+const mainHeadings = ['Unexpected guests?', 'Cooking gone wrong?', 'Game night?', 'Movie marathon?', 'Late night at office?', 'Hungry?'];
+var i = 0; // counter to loop thorugh the mainHeadings
+
 const MealsSummary = () => {
+  const [mainHeading, setMainHeading] = useState('Hungry?');
+
+  const nextHeading = useCallback(() => {
+    setMainHeading(mainHeadings[i % mainHeadings.length]);
+    i++;
+    if(i == mainHeadings.length) {
+      i = 0;
+    }
+  },[]);
+
+  useEffect(() => {
+    const intervalID = setInterval(nextHeading, 5000);
+    return () => clearInterval(intervalID);
+  }, [nextHeading]);
+
   return (
     <section className={classes.summary}>
-      <h2>Fresh & delicious food, always</h2>
+      <h2>{mainHeading}</h2>
+      <p>Order fresh & delicious food, right at your doorstep.</p>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
-      </p>
-      <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
+        We serve all different kinds of cusines. Each dish is curated & prepared by our highly-skilled chefs.
+        And we only use bio-degradeable packaging & cutlery.
       </p>
     </section>
   );
